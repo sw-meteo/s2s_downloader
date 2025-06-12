e0334109a90402c269694adb1c17f04efab4d7c3
Wen Shi <72328383+sw-meteo@users.noreply.github.com>
2025-06-12 23:33:35 +0800

    fix

--- retrieve_hind.py
+++ retrieve_hind.py
@@ -1,15 +1,31 @@
+'''
+download realtime data
+no need for hdate, only date
+'''
 import os
 from origin_info import pf_member_rltm
-from var_info import grib_code
-from basic_info import origin, fdir_root, rltm_year, basic_order, level_required, var_name_list
-from order_handler import get_all_valid_rltm_date, get_lead_str, get_var_list
+from var_info import grib_code, level_required, var_name_list
+from order_handler import get_all_valid_mvd, get_all_valid_hdate, get_lead_str, get_var_list
 from ecmwfapi import ECMWFDataServer
 server = ECMWFDataServer()
 
 
+'''
+basic info
+'''
+origin = "ecmf"
+fdir_root = "/data/sw/buffer/s2s.2020/data"
+
+order = {"class": "s2",      "dataset": "s2s",   "expver": "prod", 
+         "model": "glob",    "origin": origin,   "grid": "1.5/1.5",  
+         "stream": "enfh",   "time": "00:00:00", "format": "netcdf",       
+}
+
+
+'''
+retrieve data
+'''
 if __name__ == "__main__":
-    order = basic_order
-    
     print(f"Downloading data from {origin}")
     for ensemble_type in ["cf", "pf"]:
         print(f"retrieving {ensemble_type}")
@@ -17,28 +33,33 @@ if __name__ == "__main__":
         if ensemble_type == "pf":
             order["number"] = f"1/to/{pf_member_rltm[origin]}"
         
-        rltm_date_list = list(get_all_valid_rltm_date(origin, rltm_year))
-        for rltm_date in rltm_date_list:
-            order["date"] = rltm_date.strftime("%Y-%m-%d")
+        mvd_list = list(get_all_valid_mvd(origin))
+        for mvd in mvd_list:
+            order["date"] = mvd.strftime("%Y-%m-%d")
+            
+            hdate_list = list(get_all_valid_hdate(origin, mvd))
+            for hdate in hdate_list:
+                order["hdate"] = hdate.strftime("%Y-%m-%d")
                 
-            var_name_list = get_var_list(origin)
-            for var_name in var_name_list:
-                fname = f"{origin}.global.1P5.rltm.sd{rltm_date.strftime('%Y%m%d')}.{var_name}.{ensemble_type}.nc"
-                print(f"retrieving {fname} ...")
-                fdir = os.path.join(fdir_root, origin, var_name)
-                os.makedirs(fdir, exist_ok=True)
-                if(os.path.isfile(os.path.join(fdir, fname))):
-                    print(" -- already downloaded! skip this request. -- ")
-                    continue
-                order["target"] = os.path.join(fdir, fname)
-                
-                order["param"] = grib_code[var_name]
-                order["step"] = get_lead_str(origin, var_name)
-                
-                if (level_required[var_name] == 0):
-                    order["levtype"] = "sfc"  
-                else:
-                    order["levtype"] = "pl"
-                    order["levelist"] = '/'.join([str(lv) for lv in (level_required[var_name])])
-                
-                server.retrieve(order)
+                var_name_list = get_var_list(origin)
+                for var_name in var_name_list:
+                    fname = f"{origin}.global.1P5.mvd{mvd.strftime('%Y%m%d')}.sd{hdate.strftime('%Y%m%d')}.{var_name}.{ensemble_type}.nc"
+                    print(f"retrieving {fname} ...")
+                    fdir = os.path.join(fdir_root, origin, var_name)
+                    os.makedirs(fdir, exist_ok=True)
+                    if(os.path.isfile(os.path.join(fdir, fname))):
+                        print(" -- already downloaded! skip this request. -- ")
+                        continue
+                    order["target"] = os.path.join(fdir, fname)
+                    
+                    order["param"] = grib_code[var_name]
+                    order["step"] = get_lead_str(origin, var_name)
+                    
+                    if (level_required[var_name] == 0):
+                        order["levtype"] = "sfc"  
+                    else:
+                        order["levtype"] = "pl"
+                        order["levelist"] = '/'.join([str(lv) for lv in (level_required[var_name])])
+                    
+                    server.retrieve(order)
+
